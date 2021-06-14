b14f6b55bd36857c666f3cd4d944dcab0adf5515
Manu Varghese <manuvarghese0603@gmail.com>
2021-06-14 05:48:35 +0530

    Added documentation for CONFLIP and ZCO14003 of codechef

--- Codechef/ZCO14003.cpp
+++ Codechef/ZCO14003.cpp
@@ -3,16 +3,16 @@ using namespace std;
 #define ll long long
 int main() 
 {
-    vector<ll int> v;
+    vector<ll int> v; // Initialise vector with name v
     ll int a,n;
-    cin>>n;
+    cin>>n;             // n denotes total number of potential customers.
     for(int i=0;i<n;i++)
     {
-        cin>>a;
+        cin>>a;            // taking the budget of each cutomer and pushing them into vector v
         v.push_back(a);
     }
-    sort(v.begin(),v.end());    
+    sort(v.begin(),v.end());    // sorting the vector
     for(int i=0;i<n;i++)
-    v[i]=v[i]*(n-i);       
-    cout<<*max_element(v.begin(),v.end());
+    v[i]=v[i]*(n-i);            // multiplying each term of vector with the number of possible customer in that price range
+    cout<<*max_element(v.begin(),v.end());      // output highest value
 }
