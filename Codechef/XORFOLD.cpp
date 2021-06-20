cc40c854ec069ab5b22e5e75fc00f90878ee41c2
Manu Varghese <manuvarghese0603@gmail.com>
2021-06-21 00:40:59 +0530

    First 4 questions of June Cook-Off 2021 Division 3

---
+++ Codechef/XORFOLD.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll int t;
+    cin>>t;
+    while(t--)
+    {
+        ll int n,m,ones=0;
+        cin>>n>>m;
+        string s[n];
+        for(int i=0;i<n;i++)
+        cin>>s[i];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<s[i].size();j++)
+            if(s[i][j]=='1')
+            ones++;
+        }
+        if(ones%2!=0)
+        cout<<"YES"<<endl;
+        else 
+        cout<<"NO"<<endl;
+    }
+}
